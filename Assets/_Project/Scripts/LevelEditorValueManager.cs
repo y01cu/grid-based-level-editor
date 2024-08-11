d4b28f2b8cd1dd52bccc12eff504bd526a5cace0
y01cu <74989036+y01cu@users.noreply.github.com>
2024-08-11 23:55:10 +0300

    updated ui type button setup

--- Assets/_Project/Scripts/LevelEditorValueManager.cs
+++ Assets/_Project/Scripts/LevelEditorValueManager.cs
@@ -1,9 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelEditorValueManager : MonoBehaviour
 {
     private ColorMaterialsListSO colorMaterialsList;
     public static LevelEditorValueManager Instance { get; private set; }
+
+    public TextMeshProUGUI currentTypeText;
 
     private void Awake()
     {
